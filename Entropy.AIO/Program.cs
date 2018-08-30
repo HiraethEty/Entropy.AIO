3bf56592d4fde03d2a59ad123f7dc3e1c4bda1ad
nullopt <nullopt@outlook.com>
2018-08-30 09:52:00 +0100

    Add project files.

---
+++ Entropy.AIO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entropy.AIO
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
